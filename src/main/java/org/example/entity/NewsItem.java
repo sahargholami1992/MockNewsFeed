ede88b08b2285864df4cac37c958fb71c0cf907f
sahargholami1992 <sahar_gholami03@gmail.com>
2024-03-12 19:17:05 +0330

    add NewsItem entity

---
+++ src/main/java/org/example/entity/NewsItem.java
@@ -0,0 +1,19 @@
+package org.example.entity;
+
+import lombok.*;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class NewsItem implements Serializable {
+    private String headline;
+    private int priority;
+
+
+
+
+}
