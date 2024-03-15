2cb554a04c17cef87ca11a2db318a04a1f2364ae
sahargholami1992 <sahar_gholami03@gmail.com>
2024-03-15 18:59:10 +0330

    finally refactor

--- src/main/java/org/example/mockNewsFeed/MockNewsFeedMain.java
+++ src/main/java/org/example/mockNewsFeed/MockNewsFeedMain.java
@@ -7,7 +7,7 @@ import java.util.Timer;
 public class MockNewsFeedMain {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter news frequency in seconds: ");
+        System.out.println("enter newsFrequency in second: ");
         long frequency = scanner.nextInt();
         Timer timer = new Timer();
         timer.schedule(new SendTask(), 0, frequency * 1000);
